1181049bad97884dd86b26e421cebaca76de7aaf
Steven L. Speek <slspeek@gmail.com>
2026-08-19 14:59:53 +0200

    Add batch generation script for PDF creation from CSV data

---
+++ batch-generation.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+(tail -n +2 "$1"; echo) | \
+    while IFS=, read -r NAME TITLE EMAIL PHONE MATRIX; 
+    do make viewpdf NAME="$NAME" \
+                    TITLE="$TITLE" \
+                    EMAIL="$EMAIL" \
+                    PHONE="$PHONE" \
+                    MATRIX="$MATRIX";
+    done
